23868130aa542d785c0e2866a49eb9686ab8b3bf
mdsimmo <mdsimmo@gmail.com>
2023-02-28 18:45:12 +1100

    Form submit feedback

--- src/register-form.tsx
+++ src/register-form.tsx
@@ -1,3 +1,5 @@
+import { faCheck, faSpinner, faWarning } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Fragment } from 'react';
 import { useForm } from 'react-hook-form';
 import { api_url, Member } from './core';
@@ -7,6 +9,7 @@ type RegisterScreenProps = Record<string, never>
 
 type RegisterScreenState = {
     loggingIn: boolean,
+    status: RegisterFormState,
 }
 
 export class RegisterScreen extends React.Component<RegisterScreenProps, RegisterScreenState> {
@@ -16,6 +19,7 @@ export class RegisterScreen extends React.Component<RegisterScreenProps, Registe
 
         this.state = {
             loggingIn: false,
+            status: RegisterFormState.ENTRY,
         };
     }
 
@@ -26,7 +30,13 @@ export class RegisterScreen extends React.Component<RegisterScreenProps, Registe
                     <RegisterSplash onClick={()=>document.getElementById('form-anchor')?.scrollIntoView()}/>
                 </article>
                 <article id="form-anchor">
-                    <RegisterForm/>
+                    <RegisterForm onError={err => {
+                        this.setState({status: RegisterFormState.ERROR});
+                        console.log(err);
+                    }}
+                    onSuccess={() => this.setState({status: RegisterFormState.SUCCESS})} 
+                    onLoad={() => this.setState({status: RegisterFormState.LOADING})}
+                    status={this.state.status}/>
                 </article>
             </Fragment>
         );
@@ -40,17 +50,31 @@ type RegisterSplashProps = {
 function RegisterSplash(props: RegisterSplashProps) {
     return (
         <div>
-            <h1>Welcome to <br/> Noble Park Baptist</h1>
-            <p> Think of some words to go here</p>
+            <h1>Welcome to <br/> Noble Park Evangelical <br/> Baptist Church</h1>
             <button onClick={props.onClick}>Check In</button>
         </div>
     );
 }
 
-function RegisterForm() {
+type RegisterFormProps = {
+    onError: (err: string) => void,
+    onSuccess: () => void,
+    onLoad: () => void,
+    status: RegisterFormState,
+}
+
+enum RegisterFormState {
+    ENTRY,
+    LOADING,
+    ERROR,
+    SUCCESS,
+}
+
+function RegisterForm(props: RegisterFormProps) {
 
     const submitData = (member: Member) => {
         console.log(typeof(member.mobile));
+        props.onLoad();
         fetch(api_url("update-member"), {
             method: 'POST',
             headers: {
@@ -59,18 +83,49 @@ function RegisterForm() {
             body: JSON.stringify({
                 member: member
             })
+        })
+        .then(response => {
+            if (response.ok) {
+                props.onSuccess();
+            } else {
+                props.onError("Invalid data");
+            }
+        })
+        .catch(e => {
+            console.log(e);
+            props.onError("Network error");
         });
     };
 
     const { register, handleSubmit, formState: { errors } } = useForm<Member>();
 
+    let submit;
+    switch (props.status) {
+        case RegisterFormState.ENTRY:
+            submit = <button type="submit">Register</button>;
+            break;
+        case RegisterFormState.ERROR:
+            submit = <button type="submit"><FontAwesomeIcon className='error-icon' icon={faWarning}/></button>;
+            break;
+        case RegisterFormState.LOADING:
+            submit = <FontAwesomeIcon className='loading-icon' icon={faSpinner}/>;
+            break;
+        case RegisterFormState.SUCCESS:
+            submit = <FontAwesomeIcon className='success-icon' icon={faCheck}/>;
+            break;
+    }
+
     return (
         <form onSubmit={handleSubmit(submitData)}>
-            <label htmlFor="name">Name</label><input {...register("name")} id="name" />
-            <label htmlFor="email">Email</label><input {...register("email")} id="email" />
+            <label htmlFor="name">Name*</label><input {...register("name")} required={true} id="name" />
+            {errors.name && <p>{errors.name?.message}</p>}
+            <label htmlFor="email">Email*</label><input {...register("email")} required={true} id="email" />
+            {errors.email && <p>{errors.email?.message}</p>}
             <label htmlFor="address">Address</label><input {...register("address")} id="address" />
+            {errors.address && <p>{errors.address?.message}</p>}
             <label htmlFor="mobile">Mobile</label><input {...register("mobile", {valueAsNumber: true})} type="number" id="mobile" />
-            <button type="submit">Register</button>
+            {errors.mobile && <p>{errors.mobile?.message}</p>}
+            {submit}
         </form>
     );
 }
